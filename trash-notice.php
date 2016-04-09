a0777693106248a860d4f6c619c71ba2162b571d
Saad Ibrahim <saad.lakhany@gmail.com>
2016-04-09 14:54:11 +0400

    Updated to make sure plugin returns the correct number of days in case it has been updated in wp-config.php

--- trash-notice.php
+++ trash-notice.php
@@ -8,18 +8,28 @@
  * Text Domain: msi-trash-notice
  */
 
+function msi_get_number_days() {
+	if (defined('EMPTY_TRASH_DAYS')) {
+    	return EMPTY_TRASH_DAYS;
+	} else {
+		return "30";
+	}
+}
+
 
 // Here we update the message that shows up when user trashes a post or page.
 function msi_bulk_post_updated_messages_filter( $bulk_messages, $bulk_counts ) {
 
+	$number_days = msi_get_number_days();
+
     $bulk_messages['post'] = array(
-		'trashed'   => _n( '%s post moved to the Trash. Posts in trash are automatically deleted after 30 days.', '%s posts moved to the Trash. Posts in trash are automatically deleted after 30 days.', $bulk_counts['trashed'] ),
+		'trashed'   => _n( '%s post moved to the Trash. Posts in trash are automatically deleted after '.$number_days.' days.', '%s posts moved to the Trash. Posts in trash are automatically deleted after '.$number_days.' days.', $bulk_counts['trashed'] ),
     );
 
     return $bulk_messages;
 
     $bulk_messages['page'] = array(
-		'trashed'   => _n( '%s page moved to the Trash. Pages in trash are automatically deleted after 30 days.', '%s pages moved to the Trash. Pages in trash are automatically deleted after 30 days.', $bulk_counts['trashed'] ),
+		'trashed'   => _n( '%s page moved to the Trash. Pages in trash are automatically deleted after '.$number_days.' days.', '%s pages moved to the Trash. Pages in trash are automatically deleted after '.$number_days.' days.', $bulk_counts['trashed'] ),
     );
 
     return $bulk_messages;
@@ -30,14 +40,19 @@ add_filter( 'bulk_post_updated_messages', 'msi_bulk_post_updated_messages_filter
 
 // Here we add the notice to page that lists trashed posts or pages in the backend.
 function msi_trash_warning() {
+
+	$number_days = msi_get_number_days();
+
 	$current_screen = get_current_screen();
+
 	if($current_screen->parent_base == 'edit' && get_post_status() == 'trash') {
 		?>
 			<div class="notice-info notice">
-				<p><?php _e( 'Note: Posts in trash are automatically deleted after 30 days.', 'msi-trash-notice' ); ?></p>
+				<p><?php _e( 'Note: Posts in trash are automatically deleted after '.$number_days.' days.', 'msi-trash-notice' ); ?></p>
 			</div>
 		<?php
 	}
+	
 }
 
 add_action( 'admin_notices', 'msi_trash_warning' );
